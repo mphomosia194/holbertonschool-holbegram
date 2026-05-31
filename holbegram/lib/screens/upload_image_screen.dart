6749b15f8022cadb2843b8f7f7e0f5a2888bad96
mphomosia194 <mphomosia194@gmail.com>
2026-05-31 16:25:28 +0200

    Create initial holbegram structure

---
+++ holbegram/lib/screens/upload_image_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UploadImageScreen extends StatelessWidget {
+  const UploadImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Upload Image Screen'),
+      ),
+    );
+  }
+}
